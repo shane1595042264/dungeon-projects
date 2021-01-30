8ff1cd5a1c7a6c6c9233118b52e9e23b029a9da5
shane1595042264 <sli@cannonschool.org>
2021-01-30 14:17:48 -0500

    rooms texture update
    I drew it myself

--- src/dungeoncrawler/game/entities/Entity.java
+++ src/dungeoncrawler/game/entities/Entity.java
@@ -3,6 +3,19 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 
 import dungeoncrawler.framework.resources.
-public class Entity {
+import dungeoncrawler.framework.utils.MathHelper;
+
+public class Entity extends Rectangle{
+    private static final long serialVersionUID = 1L;
+
+    protected byte entityID;
+
+    protected boolean up;
+    protected boolean down;
+    protected boolean left;
+    protected boolean right;
+
+    protected int speed;
+    protected MathHelper.Direction facing; // a blank variable
 
 }
